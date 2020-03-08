a641b99d2312b622350aec41e636ef4ab674069f
LucHighwalker <luc.highwalker@gmail.com>
2020-03-07 20:31:37 -0800

    [Add] Added react router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { BrowserRouter, Route } from "react-router-dom";
+
 import Title from "./components/title/title";
 import Chart from "./components/chart/chart";
 import Footer from "./components/footer/footer";
@@ -10,13 +12,15 @@ import "./App.scss";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Title />
-        <div className="App_container">
-          <Chart />
+      <BrowserRouter>
+        <div className="App">
+          <Title />
+          <div className="App_container">
+            <Route path="/prices" component={Chart} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </BrowserRouter>
     );
   }
 }
