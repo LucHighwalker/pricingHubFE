4e25fbd40f9eaa7c8fb096fc44e35769579438c4
LucHighwalker <luc.highwalker@gmail.com>
2020-03-07 16:08:53 -0800

    [Add] Added chart component that fetches data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import logo from "./logo.svg";
+import Chart from "./components/chart/chart";
+
 import "./App.scss";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.jsx</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
+				<Chart />
 			</div>
 		);
 	}
---
+++ src/components/chart/chart.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./chart.scss";
+
+import { Line } from "react-chartjs-2";
+
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {}
+    };
+  }
+
+  componentDidMount() {
+    const data = {
+      title: {
+        text: "Prices over time"
+      },
+      datasets: []
+    };
+    fetch("http://127.0.0.1:4200/prices/competitor")
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        data.datasets = res.competitor;
+        console.log(data);
+        this.setState({ data });
+      });
+  }
+
+  render() {
+    return (
+      <div className="chart">
+        <Line
+          data={this.state.data}
+          options={{
+            title: { text: "This is a test" },
+            legend: {
+              display: true,
+              position: "right"
+            },
+            scales: {
+              xAxes: [
+                {
+                  title: "time",
+                  type: "time",
+                  gridLines: {
+                    lineWidth: 2
+                  },
+                  time: {
+                    parser: "MM/DD/YYYY",
+                    tooltipFormat: "ll HH:mm",
+                    unit: "day",
+                    unitStepSize: 1,
+                    displayFormats: {
+                      day: "MM/DD/YYYY"
+                    }
+                  }
+                }
+              ]
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  // action: () => dispatch(action())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chart);
